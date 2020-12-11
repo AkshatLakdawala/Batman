6ea4d38e536660c9d0a5c1bdb59105a5fa9ee7c4
AkshatLakdawala <69045916+AkshatLakdawala@users.noreply.github.com>
2020-12-11 11:15:35 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -7,10 +7,19 @@ const Body = Matter.Body;
 
 var canvas;
 var engine, world;
-var drops,maxDrops=100;
+var maxDrops=100;
 var umbrella;
+var drops=[];
+var thunder;
+var thunder1Img, thunder2Img,thunder3Img,thunder4Img;
+var rand;
+
 
 function preload(){
+   thunder1Img = loadImage("images/1.png");
+   thunder2Img = loadImage("images/2.png");
+   thunder3Img = loadImage("images/3.png")
+   thunder4Img = loadImage("images/4.png");
    
     
 }
@@ -24,11 +33,17 @@ function setup(){
 
    umbrella = new Umbrella(200,600);
 
-  
+   thunder = createSprite(random(0,400),150,40,40);
+  // thunder2.addImage(thunder1);
+   thunder.scale = 0.4;
 
-  /* for(var i=0;i<maxDrops;i++){
-      drops = new Drops(random(0,400),random(0,400));
-   }*/
+  
+   
+   for(i=0;i<maxDrops;i++){
+      
+      drops.push(new Drops(random(0,400),random(0,800)));
+   }
+
    
   
    
@@ -37,11 +52,40 @@ function setup(){
 
 function draw(){
   
-   canvas = createCanvas(600,800);
+   canvas = createCanvas(500,800);
    background("black");
-  // drops.display();
+   
    umbrella.display();
-   //drops.update();
+   
+   for (var j = 0; j <drops.length ; j++){
+     
+      drops[j].display();
+      drops[j].update();
+   }
+
+   rand = Math.round(random(1,4));
+
+   switch(rand){
+      case 1: thunder.addImage(thunder1Img);
+               break;
+      case 2: thunder.addImage(thunder2Img);
+               break;
+      case 3: thunder.addImage(thunder3Img);
+               break;
+      case 4: thunder.addImage(thunder4Img);
+               break;
+      default: break;
+   }
+
+   
+
+   drawSprites();
+
+   
+
+
+   
+   
    
   
    
